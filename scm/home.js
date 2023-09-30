fb896c04631191e5b8329d01b373658c097720f6
ricardosergiodf <39711081+ricardosergiodf@users.noreply.github.com>
2023-09-30 19:30:18 -0300

    add lugar da logo e settings, alterando cor, ajuste na função do menu ocultar

--- scm/home.js
+++ scm/home.js
@@ -1,7 +1,13 @@
 const btn_menuPrincipal = document.querySelector('#btn_menuPrincipal');
 const menuPrincipal = document.querySelector('.menuPrincipal');
-
+const todosMenusPrincipais = [...document.querySelectorAll('.btn_menuItem')];
 
 btn_menuPrincipal.addEventListener('click', (event) => {
     menuPrincipal.classList.toggle('ocultar');
 });
+
+todosMenusPrincipais.forEach(element => {
+    element.addEventListener('click', (event) => {
+        menuPrincipal.classList.add('ocultar');
+    });
+})
