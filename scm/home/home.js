ba630810e54aeeef75481c84c46d32b40fe017fc
ricardosergiodf <39711081+ricardosergiodf@users.noreply.github.com>
2023-10-01 18:59:49 -0300

    Organização

--- scm/home/home.js
+++ scm/home/home.js
@@ -1,6 +1,7 @@
 const btn_menuPrincipal = document.querySelector('#btn_menuPrincipal');
 const menuPrincipal = document.querySelector('.menuPrincipal');
 const todosMenusPrincipais = [...document.querySelectorAll('.btn_menuItem')];
+const btn_profile = document.querySelector('#btn_profile');
 
 btn_menuPrincipal.addEventListener('click', (event) => {
     menuPrincipal.classList.toggle('ocultar');
