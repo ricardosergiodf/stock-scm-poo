e9e92e3b4a2614b1a427fd68d092133dd447b7e9
ricardosergiodf <39711081+ricardosergiodf@users.noreply.github.com>
2023-10-02 23:41:19 -0300

    Adiciona produto e id ao clicar em '+ Produto'(só o texto)

--- scm/home/home.js
+++ scm/home/home.js
@@ -7,25 +7,43 @@ const btn_addProduto = document.querySelector('.btn_addProduto');
 const tela_central = document.querySelector('.tela_central');
 const form_adicionarProduto = document.querySelector('.form_adicionarProduto');
 const btn_cancelarAdd = document.querySelector('.btn_cancelarAdd');
+const input_addProdutoNome = document.querySelector('.input_addProdutoNome');
+const input_addProdutoId = document.querySelector('.input_addProdutoId');
+const btn_adicionarProduto = document.querySelector('.btn_adicionarProduto');
+
+form_adicionarProduto.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+btn_adicionarProduto.addEventListener('click', () => {
+    const elementoNomeProduto = document.createElement('p');
+    const elementoIdProduto = document.createElement('p');
+
+    elementoNomeProduto.textContent = input_addProdutoNome.value;
+    elementoIdProduto.textContent = input_addProdutoId.value;
+
+    tela_central.appendChild(elementoNomeProduto);
+    tela_central.appendChild(elementoIdProduto);
+});
 
 btn_addProduto.addEventListener('click', () => {
     form_adicionarProduto.classList.remove('ocultar');
     form_adicionarProduto.style.display = 'flex';
     btn_addProduto.classList.add('ocultar')
-})
+});
 
 btn_cancelarAdd.addEventListener('click', () => {
     form_adicionarProduto.style.display = '';
     btn_addProduto.classList.remove('ocultar');
     form_adicionarProduto.classList.add('ocultar');
-})
+});
 
 btn_menuItem.forEach(estoque => {
     estoque.addEventListener('click', () => {
         console.log(estoque);
         estoque_num.innerHTML = estoque.textContent;
-    })
-})
+    });
+});
 
 btn_menuLateral.addEventListener('click', () => {
     btn_menuLateral.classList.add('ocultar');
