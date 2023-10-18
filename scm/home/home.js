1812a7a18469f11d5e5cb983d6872c29c37f81c2
ricardosergiodf <39711081+ricardosergiodf@users.noreply.github.com>
2023-10-18 14:35:08 -0300

    Add botão de remover produto em cada produto. Resolver erro para que possa ser removido o produto ao clicar em remover

--- scm/home/home.js
+++ scm/home/home.js
@@ -10,20 +10,37 @@ const btn_cancelarAdd = document.querySelector('.btn_cancelarAdd');
 const input_addProdutoNome = document.querySelector('.input_addProdutoNome');
 const input_addProdutoId = document.querySelector('.input_addProdutoId');
 const btn_adicionarProduto = document.querySelector('.btn_adicionarProduto');
+const btn_removerProduto = document.querySelector('.btn_removerProduto');
+const produto = document.querySelector('.produto');
+const produtos = document.querySelector('.produtos');
 
 form_adicionarProduto.addEventListener('submit', function(event) {
     event.preventDefault();
 });
 
+// ERRO ABAIXO: ENTENDER O PQ DO ERRO, ARRUMAR E IMPLEMENTAR
+// produtos.forEach((produto) => {
+//     btn_removerProduto.addEventListener('click', () => {
+//         console.log("teste");
+//     });
+// });
+
 btn_adicionarProduto.addEventListener('click', () => {
     const elementoNomeProduto = document.createElement('p');
     const elementoIdProduto = document.createElement('p');
+    const elementoBtn_removerProduto = document.createElement('button');
 
     elementoNomeProduto.textContent = input_addProdutoNome.value;
     elementoIdProduto.textContent = input_addProdutoId.value;
+    elementoBtn_removerProduto.textContent = "Remover";
 
-    tela_central.appendChild(elementoNomeProduto);
-    tela_central.appendChild(elementoIdProduto);
+    const classesBtnRemover = 'btn btn-danger btn_removerProduto'; // cria uma string com o nome das classes
+    const classesBtnRemoverArray = classesBtnRemover.split(' '); // cria um array com as classes
+    elementoBtn_removerProduto.classList.add(...classesBtnRemoverArray); // adiciona o array de classes no btn passando as classes como argumentos separados usando o operador de propagação ('...')
+
+    produto.appendChild(elementoNomeProduto);
+    produto.appendChild(elementoIdProduto);
+    produto.appendChild(elementoBtn_removerProduto);
 });
 
 btn_addProduto.addEventListener('click', () => {
