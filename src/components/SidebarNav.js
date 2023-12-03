683f977300e29dcb4e09b803a08ac0fe3889e412
ricardosergiodf <39711081+ricardosergiodf@users.noreply.github.com>
2023-12-02 21:08:59 -0300

    Stocks + transferir (ñ funcionando ainda)

--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -7,9 +7,9 @@ const SidebarNav = () => {
   const { asPath } = useRouter();
 
   return (
-    <Stack spacing="6" bg="#3e3e42" h="100%">
+    <Stack spacing="8" bg="#3e3e42" h="100%">
       <Stack>
-        <Text fontSize="xs" fontWeight="bold" color="#fff" my="6">
+        <Text fontSize="xs" fontWeight="bold" color="#fff" my="8" ml="2">
           CADASTRO
         </Text>
         <Stack>
@@ -21,15 +21,28 @@ const SidebarNav = () => {
             bg={asPath === "/" ? "#3e3e42" : ""}
           >
             <Link href="/">
-              <Text fontSize="md" fontWeight="medium" color="#fff">
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
                 PRODUTOS
+              </Text>
+            </Link>
+          </ChakraLink>
+          <ChakraLink
+            _hover={{ bg: "gray" }}
+            px="4"
+            py="2"
+            borderRadius={5}
+            bg={asPath === "/" ? "#3e3e42" : ""}
+          >
+            <Link href="/stocks">  {/* FAZER O STOCKS */}
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
+                ESTOQUES
               </Text>
             </Link>
           </ChakraLink>
         </Stack>
       </Stack>
       <Stack>
-        <Text fontSize="xs" fontWeight="bold" color="#fff">
+        <Text fontSize="xs" fontWeight="bold" color="#fff" ml="2">
           ESTOQUE
         </Text>
         <Stack>
@@ -40,7 +53,7 @@ const SidebarNav = () => {
             borderRadius={5}
           >
             <Link href="/balance">
-              <Text fontSize="md" fontWeight="medium" color="#fff">
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
                 SALDO
               </Text>
             </Link>
@@ -52,7 +65,7 @@ const SidebarNav = () => {
             borderRadius={5}
           >
             <Link href="/stockEntries">
-              <Text fontSize="md" fontWeight="medium" color="#fff">
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
                 ENTRADAS
               </Text>
             </Link>
@@ -64,8 +77,20 @@ const SidebarNav = () => {
             borderRadius={5}
           >
             <Link href="/stockOutputs">
-              <Text fontSize="md" fontWeight="medium" color="#fff">
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
                 SAÍDAS
+              </Text>
+            </Link>
+          </ChakraLink>
+          <ChakraLink
+            _hover={{ bg: "gray" }}
+            px="4"
+            py="2"
+            borderRadius={5}
+          >
+            <Link href="/stockTransfer"> {/* FAZER O TRANSFERIR */}
+              <Text fontSize="md" fontWeight="medium" color="#fff" ml="2">
+                TRANSFERIR
               </Text>
             </Link>
           </ChakraLink>
